8e2ae47438512e770c05efaab268a13d2ae8ecf9
AnnoDomine <AnnoDomine@users.noreply.github.com>
2022-07-21 06:39:36 +0200

    init redux-store

--- src/index.js
+++ src/index.js
@@ -5,18 +5,23 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import { UserProvider } from "./contexts/user.context";
 import { CartProvider } from "./contexts/cart.context";
+
+import { store } from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <UserProvider>
-                <CartProvider>
-                    <App />
-                </CartProvider>
-            </UserProvider>
+            <Provider store={store}>
+                <UserProvider>
+                    <CartProvider>
+                        <App />
+                    </CartProvider>
+                </UserProvider>
+            </Provider>
         </BrowserRouter>
     </React.StrictMode>
 );
