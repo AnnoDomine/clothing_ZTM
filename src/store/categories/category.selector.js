40c5edf307d3b7a91020c80bfab3c9c6250d0e00
AnnoDomine <AnnoDomine@users.noreply.github.com>
2022-07-23 07:46:48 +0200

    init createSelector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,16 @@
-export const categoriesSelector = (state) =>
-    state.categories.categories.reduce((acc, docSnapshot) => {
+import { createSelector } from "@reduxjs/toolkit";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+);
+
+export const categoriesSelector = createSelector([selectCategories], (categories) =>
+    categories.reduce((acc, docSnapshot) => {
         const { title, items } = docSnapshot;
         acc[title.toLowerCase()] = items;
         return acc;
-    }, {});
+    }, {})
+);
