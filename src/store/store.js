ba231dcabad5a5e0b63802361c65548d8be96ee6
AnnoDomine <AnnoDomine@users.noreply.github.com>
2022-07-23 06:49:50 +0200

    add own redux middleware logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
 import { rootReducer } from "./rootReducer";
 
+const loggerMiddleware = (store) => (next) => (action) => {
+    if (!action.type) {
+        return next(action);
+    }
+    console.log("type: ", action.type);
+    console.log("payload: ", action.payload);
+    console.log("currentState: ", store.getState());
+
+    next(action);
+
+    console.log("next state: ", store.getState());
+};
+
 export const store = configureStore({
     reducer: rootReducer,
-    middleware: [logger],
+    middleware: [loggerMiddleware],
     devTools: true,
     preloadedState: undefined,
 });
