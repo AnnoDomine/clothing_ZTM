a1a2e13348297852df275d0a9f9a92c7a19db4df
AnnoDomine <AnnoDomine@users.noreply.github.com>
2022-07-23 15:05:40 +0200

    add dev safe

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
 
 import { rootReducer } from "./rootReducer";
 
-const loggerMiddleware = (store) => (next) => (action) => {
-    if (!action.type) {
-        return next(action);
-    }
-    console.log("type: ", action.type);
-    console.log("payload: ", action.payload);
-    console.log("currentState: ", store.getState());
+// const loggerMiddleware = (store) => (next) => (action) => {
+//     if (!action.type) {
+//         return next(action);
+//     }
+//     console.log("type: ", action.type);
+//     console.log("payload: ", action.payload);
+//     console.log("currentState: ", store.getState());
 
-    next(action);
+//     next(action);
 
-    console.log("next state: ", store.getState());
-};
+//     console.log("next state: ", store.getState());
+// };
 
 const persisitConfig = {
     key: "root",
@@ -25,10 +26,12 @@ const persisitConfig = {
 
 const persistedReducer = persistReducer(persisitConfig, rootReducer);
 
+const middlewareArray = [process.env.NODE_ENV !== "production" && logger];
+
 export const store = configureStore({
     reducer: persistedReducer,
-    middleware: [loggerMiddleware],
-    devTools: true,
+    middleware: middlewareArray,
+    devTools: process.env.NODE_ENV !== "production",
     preloadedState: undefined,
 });
 
